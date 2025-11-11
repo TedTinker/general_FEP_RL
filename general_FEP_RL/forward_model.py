657e5d39769b54f9bd3816e0f8b2e2efd7a557ab
Ted Tinker <tedjtinker@gmail.com>
2025-11-11 21:06:32 +0900

    trying to make this a package

--- general_FEP_RL/forward_model.py
+++ general_FEP_RL/forward_model.py
@@ -225,6 +225,7 @@ class Forward_Model(nn.Module):
         for key in action_dict.keys():
             self.action_dict[key] = nn.ModuleDict()
             self.action_dict[key]["encoder"] = action_dict[key]["encoder"](verbose = verbose)
+            self.action_dict[key]["decoder"] = action_dict[key]["decoder"](entropy = True, verbose = verbose)
         
         encoded_action_size = 0 
         for key, value in self.action_dict.items():
