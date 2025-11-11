e663424e04a96e6f8cf9787144706b2fe42d29f8
Ted Tinker <tedjtinker@gmail.com>
2025-11-12 08:52:27 +0900

    trying to make this a package

--- general_FEP_RL/decoders/decode_image.py
+++ general_FEP_RL/decoders/decode_image.py
@@ -67,8 +67,8 @@ class Decode_Image(nn.Module):
         a = a.reshape(episodes * steps, 16, 28, 28)
         output, log_prob = self.mu_std(a)
         output = (output + 1) / 2
-        [output, log_prob] = model_end(episodes, steps, [(output, "cnn"), (log_prob, "cnn")])
-        print("log_prob:", output.shape) # I THINK THIS IS BAD. It should be (episodes, steps, 1)
+        [output] = model_end(episodes, steps, [(output, "cnn")])
+        print("output:", output.shape) # I THINK THIS IS BAD. It should be (episodes, steps, 1)
         print("log_prob:", log_prob.shape) # I THINK THIS IS BAD. It should be (episodes, steps, 1)
         output = output.reshape(episodes, steps, 28, 28, 1)
         return(output, log_prob)
