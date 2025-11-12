46c7df4cdf5ff3fe502b0eb6b0f5c2f74afd4d09
Ted Tinker <tedjtinker@gmail.com>
2025-11-12 21:14:01 +0900

    trying to make this a package

--- general_FEP_RL/decoders/decode_description.py
+++ general_FEP_RL/decoders/decode_description.py
@@ -65,6 +65,7 @@ class Decode_Description(nn.Module):
     
     
     def loss_func(self, true_values, predicted_values):
+        print(true_values.shape, predicted_values.shape)
         loss_value = F.mse_loss(predicted_values, true_values, reduction = None)
         return loss_value
     
