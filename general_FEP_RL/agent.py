4a722b6c9a615b4469d27506d5ec43e6000e11e9
Ted Tinker <tedjtinker@gmail.com>
2025-11-16 16:22:29 +0900

    trying to make this a package

--- general_FEP_RL/agent.py
+++ general_FEP_RL/agent.py
@@ -174,7 +174,7 @@ class Agent:
         for key, value in self.observation_dict.items():
             eta = self.observation_dict[key]["eta"]
             obs_curiosity = torch.clamp(complexities[key], min = 0, max = 1) * eta
-            curiosities[key] = obs_curiosity
+            curiosities[key] = obs_curiosity.mean()
             curiosity = curiosity + obs_curiosity
         total_reward = reward + curiosity
 
