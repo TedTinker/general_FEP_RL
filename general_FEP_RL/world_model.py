d3a5fc12a817a9eb7721af1eddccf47e4722c373
Ted Tinker <tedjtinker@gmail.com>
2025-12-04 17:04:51 +0900

    trying to make this a package

--- general_FEP_RL/world_model.py
+++ general_FEP_RL/world_model.py
@@ -122,8 +122,8 @@ class World_Model_Layer(nn.Module):
         else:
             self.zp_zq_dict["zq"] = ZP_ZQ(
                 zp_in_features = hidden_state_size, 
-                zq_in_features = hidden_state_size + lower_zp_zq_size, 
-                zp_zq_sizes = hidden_state_size)
+                zq_in_features = hidden_state_size + lower_zp_zq_size[0], 
+                zp_zq_sizes = [hidden_state_size])
     
         if(top_level):
             higher_hidden_state_size = 0
