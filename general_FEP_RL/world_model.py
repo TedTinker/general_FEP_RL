48b75890e31f929fb3702c2b940ea03990f87138
Ted Tinker <tedjtinker@gmail.com>
2025-12-04 17:16:38 +0900

    trying to make this a package

--- general_FEP_RL/world_model.py
+++ general_FEP_RL/world_model.py
@@ -145,9 +145,10 @@ class World_Model_Layer(nn.Module):
     def forward(
             self, 
             prev_hidden_state, 
-            encoded_obs, 
-            encoded_prev_action,
-            higher_hidden_state,
+            encoded_obs = None, 
+            encoded_prev_action = None,
+            higher_hidden_state = None,
+            lower_zp_zq = None,
             ):
         
         def reshape(inputs, episodes, steps):
@@ -352,6 +353,10 @@ class World_Model(nn.Module):
     
     # This was originally made to utilize multiple layers, which is not currently implemented.
     def bottom_to_top_step(self, prev_hidden_states, obs, prev_action):
+        
+        for i in len(prev_hidden_states):
+            print("hidden state", i)
+        
         new_hidden_states_p, new_hidden_states_q, inner_state_dict = \
             self.wl(
                 prev_hidden_state = prev_hidden_states, 
