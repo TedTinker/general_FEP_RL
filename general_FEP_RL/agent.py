c93316cdb16a0496bd58a9dad73bcf0bf17f3f32
Ted Tinker <tedjtinker@gmail.com>
2025-12-12 14:40:53 +0900

    trying to make this a package

--- general_FEP_RL/agent.py
+++ general_FEP_RL/agent.py
@@ -232,7 +232,7 @@ class Agent:
                 
         # Train critics
         with torch.no_grad():
-            new_action_dict, new_log_pis_dict, imitation_loss = self.actor(hq[0][:, 1:-1].detach(), best_action)
+            new_action_dict, new_log_pis_dict, imitation_loss = self.actor(hq[0][:, 1:].detach(), best_action)
             
             Q_target_nexts = []
             for i in range(len(self.critics)):
