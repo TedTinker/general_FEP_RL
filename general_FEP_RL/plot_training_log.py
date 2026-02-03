22dc556e012c6e2f7ebabe352ece2f4ea87b06ee
TedTinker <tedjtinker@gmail.com>
2026-02-03 13:50:15 +0900

    searching for one-off-issues

--- general_FEP_RL/plot_training_log.py
+++ general_FEP_RL/plot_training_log.py
@@ -39,7 +39,7 @@ def plot_training_log(training_log, folder="", epoch = 0):
     plt.close()
     
     plt.figure(figsize=(6, 6))
-    plt.plot(epoch_nums, ["Q_for_actor"], label="-Q_for_actor")
+    plt.plot(epoch_nums, training_log["Q_for_actor"], label="-Q_for_actor")
     plt.plot(epoch_nums, training_log["entropy_for_actor"], label="-entropy_for_actor")
     plt.plot(epoch_nums, training_log["total_imitation_loss"], label="-total_imitation_loss")
     plt.plot(epoch_nums, training_log["actor_loss"], label="actor_loss")
