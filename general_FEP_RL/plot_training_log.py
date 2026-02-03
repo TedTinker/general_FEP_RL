d8775ff5c5acde49b128a78b0858d8d26d18a05d
TedTinker <tedjtinker@gmail.com>
2026-02-03 13:47:01 +0900

    searching for one-off-issues

--- general_FEP_RL/plot_training_log.py
+++ general_FEP_RL/plot_training_log.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 
 def plot_training_log(training_log, folder="", epoch = 0):
+    
+    epoch_nums = training_log["epoch_num"]
         
     plt.figure(figsize=(6, 6))
     for key, value in training_log["accuracy_losses"].items():
-        plt.plot(value, label=f"accuracy loss {key}")
+        plt.plot(epoch_nums, value, label=f"accuracy loss {key}")
     for key, value in training_log["complexity_losses"].items():
-        plt.plot(value, label=f"complexity loss {key}")
+        plt.plot(epoch_nums, value, label=f"complexity loss {key}")
     plt.title(f"Losses for Accuracy and Complexity over epochs")
     plt.xlabel("Epoch")
-    plt.ylabel(key)
+    plt.ylabel("Value")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
@@ -20,13 +22,13 @@ def plot_training_log(training_log, folder="", epoch = 0):
     plt.close()
     
     plt.figure(figsize=(6, 6))
-    plt.plot(training_log["average_reward"], label="reward")
+    plt.plot(epoch_nums, training_log["average_reward"], label="reward")
     for key, value in training_log["curiosities"].items():
-        plt.plot(value, label=f"curiosity {key}")
-    plt.plot(training_log["total_reward"], label="total")
+        plt.plot(epoch_nums, value, label=f"curiosity {key}")
+    plt.plot(epoch_nums, training_log["total_reward"], label="total")
     plt.title(f"Rewards over epochs")
     plt.xlabel("Epoch")
-    plt.ylabel(key)
+    plt.ylabel("Value")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
@@ -37,13 +39,13 @@ def plot_training_log(training_log, folder="", epoch = 0):
     plt.close()
     
     plt.figure(figsize=(6, 6))
-    plt.plot(training_log["Q_for_actor"], label="Q_for_actor")
-    plt.plot(training_log["entropy_for_actor"], label="entropy_for_actor")
-    plt.plot(training_log["total_imitation_loss"], label="total_imitation_loss")
-    plt.plot(training_log["actor_loss"], label="actor_loss")
+    plt.plot(epoch_nums, ["Q_for_actor"], label="-Q_for_actor")
+    plt.plot(epoch_nums, training_log["entropy_for_actor"], label="-entropy_for_actor")
+    plt.plot(epoch_nums, training_log["total_imitation_loss"], label="-total_imitation_loss")
+    plt.plot(epoch_nums, training_log["actor_loss"], label="actor_loss")
     plt.title(f"Actor loss over epochs")
     plt.xlabel("Epoch")
-    plt.ylabel(key)
+    plt.ylabel("Value")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
@@ -54,12 +56,14 @@ def plot_training_log(training_log, folder="", epoch = 0):
     plt.close()
     
     plt.figure(figsize=(6, 6))
-    plt.plot(training_log["Q_target"], label="Q Target")
+    plt.plot(epoch_nums, training_log["Q_target"], label="Q Target")
     for i, critic_loss in enumerate(training_log["critic_predictions"]):
-        plt.plot(critic_loss, label=f"Critic Predictions")
+        plt.plot(epoch_nums, critic_loss, label=f"Critic {i} Predictions")
+    for i, loss in enumerate(training_log["critic_losses"]):
+        plt.plot(epoch_nums, loss, label=f"Critic {i} Loss")
     plt.title(f"Critic predictions over epochs")
     plt.xlabel("Epoch")
-    plt.ylabel(key)
+    plt.ylabel("Value")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
@@ -71,10 +75,10 @@ def plot_training_log(training_log, folder="", epoch = 0):
     
     plt.figure(figsize=(6, 6))
     for key, alpha in training_log["alphas"].items():
-        plt.plot(alpha, label=f"Alpha {key}")
+        plt.plot(epoch_nums, alpha, label=f"Alpha {key}")
     plt.title(f"Alphas over epochs")
     plt.xlabel("Epoch")
-    plt.ylabel(key)
+    plt.ylabel("Value")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
