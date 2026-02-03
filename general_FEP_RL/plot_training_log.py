2f2109aa4fab2200ba41ac80190903e9914d08e2
TedTinker <tedjtinker@gmail.com>
2026-02-03 14:52:12 +0900

    searching for one-off-issues

--- general_FEP_RL/plot_training_log.py
+++ general_FEP_RL/plot_training_log.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 
-def plot_training_log(training_log, folder="", epoch = 0):
+def plot_training_log(agent, folder="", epoch = 0):
+    
+    training_log = agent.training_log 
+    training_log_actor = agent.training_log_actor
     
     epoch_nums = training_log["epoch_num"]
+    epoch_nums_actor = training_log_actor["epoch_num"]
         
     plt.figure(figsize=(6, 6))
     for key, value in training_log["accuracy_losses"].items():
@@ -39,10 +43,10 @@ def plot_training_log(training_log, folder="", epoch = 0):
     plt.close()
     
     plt.figure(figsize=(6, 6))
-    plt.plot(epoch_nums, training_log["Q_for_actor"], label="-Q_for_actor")
-    plt.plot(epoch_nums, training_log["entropy_for_actor"], label="-entropy_for_actor")
-    plt.plot(epoch_nums, training_log["total_imitation_loss"], label="-total_imitation_loss")
-    plt.plot(epoch_nums, training_log["actor_loss"], label="actor_loss")
+    plt.plot(epoch_nums_actor, training_log_actor["Q_for_actor"], label="-Q_for_actor")
+    plt.plot(epoch_nums_actor, training_log_actor["entropy_for_actor"], label="-entropy_for_actor")
+    plt.plot(epoch_nums_actor, training_log_actor["total_imitation_loss"], label="-total_imitation_loss")
+    plt.plot(epoch_nums_actor, training_log_actor["actor_loss"], label="actor_loss")
     plt.title(f"Actor loss over epochs")
     plt.xlabel("Epoch")
     plt.ylabel("Value")
