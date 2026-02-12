518cdfbbd39764515b42a9b276d545e802cccb73
TedTinker <tedjtinker@gmail.com>
2026-02-12 16:37:27 +0900

    trying locally

--- general_FEP_RL/agent.py
+++ general_FEP_RL/agent.py
@@ -537,6 +537,7 @@ class Agent:
     def excess_epoch_numbers(self, log):
         if log == {}:
             return None
+        print(log.keys())
         epoch_numbers = log['epoch_num']
         if len(epoch_numbers) < self.max_epochs_in_log:
             return None
