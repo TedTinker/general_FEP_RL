dc05fa0735f83fb16aeb453555e89e0dbded2f7f
Ted <TedJTinker@gmail.com>
2026-03-25 15:08:28 +0900

    Flipping DKL(p||q)

--- general_FEP_RL/world_model.py
+++ general_FEP_RL/world_model.py
@@ -93,7 +93,7 @@ class ZP_ZQ(nn.Module):
         else:
             zp = zp_mu 
             zq = zq_mu
-        dkl = calculate_dkl(zq_mu, zq_std, zp_mu, zp_std)
+        dkl = calculate_dkl(zp_mu, zp_std, zq_mu, zq_std)
         return {
             'zp' : zp, 
             'zq' : zq, 
